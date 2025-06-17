8fd321a6c0b172864eb5224c28c8584092437856
Luluh <lulualmogbil@gmail.com>
2025-06-17 14:49:15 +0300

    bonus file solved

---
+++ bonus.py
@@ -0,0 +1,20 @@
+#Luluh Almogbil
+#bonus file
+
+sen1 = "My name is Luluh Almogbil, I am computer science fresh graduate from jubail indusrtial college"
+word = "Luluh"
+
+#length of a sentence
+print (len(sen1))
+#index of the first occurrence of the word in the sentence
+print (sen1.index(word))
+#number of times the word appeard in a sentence
+print (sen1.count(word))
+#sentence in uppercase
+print (sen1.upper())
+#lowercase
+print (sen1.lower())
+#replace word with other one of my choice
+print (sen1.replace("science","scientist"))
+#last char in the sentence
+print (sen1[-1])
