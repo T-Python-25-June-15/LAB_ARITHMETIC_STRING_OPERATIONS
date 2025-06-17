a07e703f57053cf6e8d9c17c1aa20567957c9fe5
Sarah <sarahalsmari1999@gmail.com>
2025-06-17 14:21:12 +0300

    Created a Python script to perform various string operations

---
+++ LabBonus.py
@@ -0,0 +1,10 @@
+sentence = "In Python, all data is represented as objects in memory clearly"
+word = "Python"
+
+print(len(sentence))
+print(sentence.index(word))
+print(sentence.count(word))
+print(sentence.upper())
+print(sentence.lower())
+print(sentence.replace(word,"Ruby"))
+print(sentence[-1])
