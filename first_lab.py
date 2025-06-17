05729752a0da9b77b14d495510839b7e360073b6
shahad-bf <shoshomohd393@gmail.com>
2025-06-17 14:20:34 +0300

    bouns exersice

--- first_lab.py
+++ first_lab.py
@@ -1,23 +1,27 @@
-price=float(input("Enter the price of Item:"))
-quantity=int(input("Enter the auantity:"))
-task_rate_percentage=float(input("Enter the task_rate(%):"))
 
-task_rate=task_rate_percentage/100
+sentence = "My name is Shahad, I Learning Python is so fun in tuwaiq"
 
-
-subtotla= price*quantity
-task= subtotla * task_rate
-total= subtotla + task
-
-print("\n Receipt")
-print("price: ${:.2f}".format(price))
-print("quantity: {}".format(quantity))
-print("task rate:{:.1f}%".format(task_rate_percentage))
-
-print("\n")
-subtotal = price * quantity     
+word = "Python"
 
 
-print("Subtotal: ${:.2f}".format(subtotal)) 
-print("Tax: ${:.2f}".format(task))
-print("Total: ${:.2f}".format(total))
+print("Length of the sentence:", len(sentence))
+
+print("First occurrence of the word:", sentence.find(word))
+print("Number of times the word appears:", sentence.count(word))
+
+
+print("Sentence in UPPERCASE:", sentence.upper())
+
+
+
+
+print("Sentence in lowercase:", sentence.lower())
+
+
+new_sentence = sentence.replace(word, "Java")
+print("Sentence after replacement:", new_sentence)
+
+
+
+print("Last character of the sentence:", sentence[-1])
+
